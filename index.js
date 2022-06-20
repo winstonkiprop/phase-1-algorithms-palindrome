12cfb8bb70e39950d7209236e7bb0c2face61a6f
winston kiprop <winstonekip50@gmail.com>
2022-06-20 11:33:36 +0300

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  let l = word.length;
+  for (let i = 0; i < l/2; i++) {
+   if (word[i] !== word[l - 1 - i]) {
+    return false;
+   }
+  }
+  return true;
+  }
+  
 
 /* 
   Add your pseudocode here
-*/
+// write a function called is palindrome & pass one parameter
+//use a for loop to check to iterate over the characters of the word
+use an if statement to check if the first and the last characters of the word if they match
+if they match === the word is palindrome (true)
+else !===palindrome(false)
 
 /*
   Add written explanation of your solution here
